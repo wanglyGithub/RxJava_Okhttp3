d58e7cf15471c0c607c841c5f218a6df23c68643
wanglyGithub <wlytctw@163.com>
2016-11-04 14:58:13 +0800

    提交JavaBean

---
+++ app/src/main/java/com/wangly/rxandroid/bean/UserInfo.java
@@ -0,0 +1,244 @@
+package com.wangly.rxandroid.bean;
+
+/**
+ * Created by wangly on 2016/11/4.
+ */
+
+public class UserInfo {
+
+
+    private String userid;
+    private String nickname;
+    private String height;
+    private String weight;
+    private String sex;
+    private String city;
+    private double bmi;
+    private String totlestep;
+    private String millionstepday;
+    private String totaltime;
+    private String zmrate;
+    private String beststeprate;
+    private String allrate;
+    private String continuousmillion;
+    private String dayaveragestep;
+    private String paimingkey;
+    private String continuenum;
+    private String wb_days_rank;
+    private String wb_hours_rank;
+    private String wb_faststep_rank;
+    private String wb_zmrate_rank;
+    private String wb_rate_rank;
+    private String wb_totlestep_rank;
+    private int fatconsumed;
+    private int stepnumber;
+    private int calorieconsumed;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getBmi() {
+        return bmi;
+    }
+
+    public void setBmi(double bmi) {
+        this.bmi = bmi;
+    }
+
+    public String getTotlestep() {
+        return totlestep;
+    }
+
+    public void setTotlestep(String totlestep) {
+        this.totlestep = totlestep;
+    }
+
+    public String getMillionstepday() {
+        return millionstepday;
+    }
+
+    public void setMillionstepday(String millionstepday) {
+        this.millionstepday = millionstepday;
+    }
+
+    public String getTotaltime() {
+        return totaltime;
+    }
+
+    public void setTotaltime(String totaltime) {
+        this.totaltime = totaltime;
+    }
+
+    public String getZmrate() {
+        return zmrate;
+    }
+
+    public void setZmrate(String zmrate) {
+        this.zmrate = zmrate;
+    }
+
+    public String getBeststeprate() {
+        return beststeprate;
+    }
+
+    public void setBeststeprate(String beststeprate) {
+        this.beststeprate = beststeprate;
+    }
+
+    public String getAllrate() {
+        return allrate;
+    }
+
+    public void setAllrate(String allrate) {
+        this.allrate = allrate;
+    }
+
+    public String getContinuousmillion() {
+        return continuousmillion;
+    }
+
+    public void setContinuousmillion(String continuousmillion) {
+        this.continuousmillion = continuousmillion;
+    }
+
+    public String getDayaveragestep() {
+        return dayaveragestep;
+    }
+
+    public void setDayaveragestep(String dayaveragestep) {
+        this.dayaveragestep = dayaveragestep;
+    }
+
+    public String getPaimingkey() {
+        return paimingkey;
+    }
+
+    public void setPaimingkey(String paimingkey) {
+        this.paimingkey = paimingkey;
+    }
+
+    public String getContinuenum() {
+        return continuenum;
+    }
+
+    public void setContinuenum(String continuenum) {
+        this.continuenum = continuenum;
+    }
+
+    public String getWb_days_rank() {
+        return wb_days_rank;
+    }
+
+    public void setWb_days_rank(String wb_days_rank) {
+        this.wb_days_rank = wb_days_rank;
+    }
+
+    public String getWb_hours_rank() {
+        return wb_hours_rank;
+    }
+
+    public void setWb_hours_rank(String wb_hours_rank) {
+        this.wb_hours_rank = wb_hours_rank;
+    }
+
+    public String getWb_faststep_rank() {
+        return wb_faststep_rank;
+    }
+
+    public void setWb_faststep_rank(String wb_faststep_rank) {
+        this.wb_faststep_rank = wb_faststep_rank;
+    }
+
+    public String getWb_zmrate_rank() {
+        return wb_zmrate_rank;
+    }
+
+    public void setWb_zmrate_rank(String wb_zmrate_rank) {
+        this.wb_zmrate_rank = wb_zmrate_rank;
+    }
+
+    public String getWb_rate_rank() {
+        return wb_rate_rank;
+    }
+
+    public void setWb_rate_rank(String wb_rate_rank) {
+        this.wb_rate_rank = wb_rate_rank;
+    }
+
+    public String getWb_totlestep_rank() {
+        return wb_totlestep_rank;
+    }
+
+    public void setWb_totlestep_rank(String wb_totlestep_rank) {
+        this.wb_totlestep_rank = wb_totlestep_rank;
+    }
+
+    public int getFatconsumed() {
+        return fatconsumed;
+    }
+
+    public void setFatconsumed(int fatconsumed) {
+        this.fatconsumed = fatconsumed;
+    }
+
+    public int getStepnumber() {
+        return stepnumber;
+    }
+
+    public void setStepnumber(int stepnumber) {
+        this.stepnumber = stepnumber;
+    }
+
+    public int getCalorieconsumed() {
+        return calorieconsumed;
+    }
+
+    public void setCalorieconsumed(int calorieconsumed) {
+        this.calorieconsumed = calorieconsumed;
+    }
+}
